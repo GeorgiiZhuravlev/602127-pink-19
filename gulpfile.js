0f4ad715ad2690c49bded0a9f8bfab4d119fe1f3
GeorgeShadoof <58880355+GeorgiiZhuravlev@users.noreply.github.com>
2020-04-17 17:58:04 +0300

    Улучшение стилизации.

--- gulpfile.js
+++ gulpfile.js
@@ -23,7 +23,7 @@ gulp.task("css", function () {
 
 gulp.task("server", function () {
   server.init({
-    server: { baseDir: "source/", index: "form.html" },
+    server: { baseDir: "source/", index: "index.html" },
     notify: false,
     open: true,
     cors: true,
